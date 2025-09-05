664f0c926f5b34847c17f8ea0459a8061166d583
Bns134 <bnsbunganisa@gmail.com>
2025-09-05 21:48:43 +0700

    Fix: Memastikan kunci API terinstal

--- api/create-xendit-invoice.js
+++ api/create-xendit-invoice.js
@@ -3,7 +3,7 @@ const { Xendit } = require('xendit-node');
 const xenditClient = new Xendit({
   secretKey: process.env.XENDIT_SECRET_KEY,
 });
-
+console.log('Xendit Secret Key:', process.env.XENDIT_SECRET_KEY ? 'Berhasil terbaca!' : 'Gagal terbaca!');
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ message: 'Method Not Allowed' });
