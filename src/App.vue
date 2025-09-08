7b37386a20cc772d4408c12c0be8c3b9e6ee47ca
Bns134 <bnsbunganisa@gmail.com>
2025-09-08 19:30:27 +0700

    Fix: Memperbaiki panggilan fungsi tombol langganan

--- src/App.vue
+++ src/App.vue
@@ -7096,7 +7096,7 @@ watch(activePage, (newPage) => {
                         <li>✔️ Update berkala</li>
                     </ul>
                 </div>
-                <button @click="handleSubscriptionMidtrans('bulanan')"
+                <button @click="handleSubscriptionTripay('bulanan')"
                         :disabled="isSubscribingMonthly"
                         class="mt-8 w-full bg-indigo-600 text-white font-bold py-3 px-8 rounded-lg hover:bg-indigo-700 disabled:bg-indigo-400 disabled:cursor-not-allowed">
                     <span v-if="isSubscribingMonthly">Memproses...</span>
@@ -7117,7 +7117,7 @@ watch(activePage, (newPage) => {
                         <li>💰 <span class="font-semibold">Diskon 2 bulan!</span></li>
                     </ul>
                 </div>
-                <button @click="handleSubscriptionMidtrans('tahunan')"
+                <button @click="handleSubscriptionTripay('tahunan')"
                         :disabled="isSubscribingYearly"
                         class="mt-8 w-full bg-slate-800 text-white font-bold py-3 px-8 rounded-lg hover:bg-slate-700 disabled:bg-slate-400 disabled:cursor-not-allowed">
                     <span v-if="isSubscribingYearly">Memproses...</span>
