5603c849efd59bed6f3e694cb8acd1243ec4ef99
Bns134 <bnsbunganisa@gmail.com>
2025-09-09 20:32:09 +0700

    refactor: Update notification text for missing product models

--- src/App.vue
+++ src/App.vue
@@ -2562,7 +2562,7 @@ function showModal(type, data = {}) {
             }
         } else if (type === 'addProduksi') {
             if (!state.settings.modelProduk || state.settings.modelProduk.length === 0) {
-                alert("Data 'Model Produk' belum selesai dimuat. Coba lagi sesaat.");
+                alert("Data 'Model Produk' belum selesai dimuat. silahkan kehalaman pengaturan untuk menambahkan model produk.");
                 hideModal();
                 return;
             }
