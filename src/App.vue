5b21ff8283afd0d6c27b82ded15523db44c7951c
Bns134 <bnsbunganisa@gmail.com>
2025-09-25 11:50:56 +0700

    feat: tambahkan kontrol langganan pada halaman Promosi

--- src/App.vue
+++ src/App.vue
@@ -6854,10 +6854,10 @@ watch(activePage, (newPage) => {
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor"><path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z" clip-rule="evenodd" /></svg>
                         Informasi
                     </button>
-                    <button @click="saveData" :disabled="isSaving" class="bg-green-600 text-white font-bold py-2.5 px-5 rounded-lg hover:bg-green-700 transition-colors shadow disabled:bg-green-400">
-                        <span v-if="isSaving">Menyimpan...</span>
-                        <span v-else>Simpan Semua Perubahan</span>
-                    </button>
+                    <button @click="saveData" :disabled="isSaving || !isSubscriptionActive" class="bg-green-600 text-white font-bold py-2.5 px-5 rounded-lg hover:bg-green-700 transition-colors shadow disabled:bg-green-400 disabled:shadow-none">
+    <span v-if="isSaving">Menyimpan...</span>
+    <span v-else>Simpan Semua Perubahan</span>
+</button>
                 </div>
             </div>
 
