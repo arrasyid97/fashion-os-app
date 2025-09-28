ef626417a31723b613425f4d25e2b3f9db86f715
Bns134 <bnsbunganisa@gmail.com>
2025-09-28 15:40:22 +0700

    refactor: Hapus fungsi computed duplikat

--- src/App.vue
+++ src/App.vue
@@ -12912,7 +12912,7 @@ watch(activePage, (newPage) => {
         <button @click="hideModal" class="bg-slate-200 text-slate-800 font-bold py-2 px-4 rounded-lg hover:bg-slate-300">Tutup</button>
     </div>
  </div>
-</div>
+
 </template>
 
 <style scoped>
